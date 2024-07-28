e1cdeb684d392062f95ae7107a20c49fa5d163a9
Nayab-Sarfaraj <nayabsarfaraj@gmail.com>
2024-07-28 10:24:23 +0530

    second

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectToDB = async () => {
   try {
-     await mongoose.connect(
-      `mongodb+srv://${process.env.ATLAS_username}:${process.env.password}@cluster0.peuugea.mongodb.net/deploy`
+    await mongoose.connect(
+      `mongodb+srv://${process.env.ATLAS_username}:${process.env.password}@cluster0.peuugea.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     );
     console.log("ss");
   } catch (error) {
